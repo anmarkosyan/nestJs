d6916aa9273712daf222613b49ba9a72cfe18a35
Anush Markosyan <Anush_Markosyan@epam.com>
2021-11-30 15:54:33 +0400

    added all entity related features

--- mycarvalue-app/src/users/user.entity.ts
+++ mycarvalue-app/src/users/user.entity.ts
@@ -1,13 +1,35 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import {Exclude} from "class-transformer";
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid')
   id: number;
 
   @Column()
   email: string;
 
   @Column()
+  @Exclude()
   password: string;
+
+  @AfterInsert()
+  insertLogger(){
+    console.log(`Inserted record with id: ${this.id}`);
+  }
+  @AfterRemove()
+  removeLogger(){
+    console.log(`Removed record with id: ${this.id}`);
+  }
+  @AfterUpdate()
+  updateLogger(){
+    console.log(`Updated record with id: ${this.id}`);
+  }
 }
--- mycarvalue-app/src/users/users.controller.ts
+++ mycarvalue-app/src/users/users.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Param,
+  Query,
+  Delete,
+  NotFoundException,
+    ClassSerializerInterceptor,
+    UseInterceptors,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    return this.userService.create(body.email, body.password);
+  }
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get('/:id')
+  async getUser(@Param('id') id: string) {
+    const user = await this.userService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('user not found!!!');
+    }
+    return user;
+  }
+
+  @Get('')
+  getAllUsers(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userService.update(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    return this.userService.remove(parseInt(id));
+  }
+}
